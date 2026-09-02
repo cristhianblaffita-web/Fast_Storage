a46e6779eccbc85e1ed0e9e6dc408ad204d5e1bc
cristhianblaffita-web <cristhianblaffita@gmail.com>
2026-09-02 19:01:28 -0400

    fix(models): solve wrong validation schema in ProdcutInput

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel
 class ProductInput(BaseModel):
     name: str
     description: str
-    price: str
-    quantity: str
+    price: float
+    quantity: int
 
 class ProductOutput(ProductInput):
     id: int
