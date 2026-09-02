91680ce43bc46bc5d1c616d9922bca8674633b37
cristhianblaffita-web <cristhianblaffita@gmail.com>
2026-09-02 18:54:29 -0400

    feature(main.py):  Add the '/products' enpoint which will provide all the products in the database

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import sqlite3
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from database import init_table
-
+from database import init_table, use_db, session, cu
+from models import ProductOutput
 app = FastAPI()
 
 app.add_middleware(
@@ -16,6 +17,25 @@ app.add_middleware(
 def health():
     return {"health_status": "OK"}
 
+@app.get("/products")
+async def get_all_products(db: sqlite3.Connection = Depends(use_db)):
+    cu.execute("SELECT * FROM Products")
+    db_products = cu.fetchall()
+
+    table_columns = cu.execute("PRAGMA table_info('Products')").fetchall()
+
+    products = {
+        "column_names": [col[1] for col in table_columns],
+        "data": [ProductOutput(
+            id=product[0],
+            name=product[1],
+            description=product[2],
+            price=product[3],
+            quantity=product[4]
+        ) for product in db_products]
+    }
+
+    return products
 
 if __name__=="__main__":
     init_table()
