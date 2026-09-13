845cd05e5d141c614cfd80c73fa1242101abf823
cristhianblaffita-web <cristhianblaffita@gmail.com>
2026-09-12 21:20:53 -0400

    feature: Mount de App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,42 @@
+import { useProductContext } from "./hooks/useProductContext";
+import ProductsListStateManager from "./components/products_list/ProductsListStateManager";
+import ProductsList from "./components/products_list/ProductsList";
+import ProductForm from "./components/products_form/ProductsForm";
+
 function App() {
-  return (<></>)
+  const {
+    productsResponse,
+    totalProducts,
+    isLoading,
+    error,
+    handleDeleteProduct,
+    handleAddProduct
+  } = useProductContext();
+
+  const loadingCompo = (
+    <div className="notification bg-secondary text-white text-sm p-4 rounded-lg shadow-2xl w-[80vw]">
+      Fetching products. Please wait.
+    </div>
+  );
+  const errorCompo = <div>{`${error}`}</div>;
+
+  return (
+    <main className="grid grid-cols-[1fr] gap-8 text-sm">
+      <ProductForm addProductFunc={handleAddProduct} />
+      <ProductsListStateManager
+        processLoading={isLoading}
+        loadingComponent={loadingCompo}
+        processError={error}
+        errorComponent={errorCompo}
+      >
+        <ProductsList
+          productsResponse={productsResponse}
+          totalProducts={totalProducts}
+          handleDeleteProduct={handleDeleteProduct}
+        />
+      </ProductsListStateManager>
+    </main>
+  );
 }
 
-export default App
+export default App;
